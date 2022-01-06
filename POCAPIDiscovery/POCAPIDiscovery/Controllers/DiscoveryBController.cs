0ce490d0061bebf67ae5d1aa408cde0bf0361c61
Rodrigo Grechi <rodrigovincoleto@gmail.com>
2022-01-06 11:45:13 -0300

    Added .net project for simulate microservices

---
+++ POCAPIDiscovery/POCAPIDiscovery/Controllers/DiscoveryBController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using RestSharp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace POCBPIDiscovery.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class DiscoveryBController : ControllerBase
+    {
+        private readonly ILogger<DiscoveryBController> _logger;
+
+        public DiscoveryBController(ILogger<DiscoveryBController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public string Get()
+        {
+            _logger.LogInformation("begin discovery");
+            Console.WriteLine("Service Online");
+            _logger.LogInformation("end discovery");
+            return "Service Online";
+        }
+    }
+}
---
+++ POCAPIDiscovery/POCAPIDiscovery/Controllers/DiscoveryCController.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using RestSharp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace POCCPIDiscovery.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class DiscoveryCController : ControllerBase
+    {
+        private readonly ILogger<DiscoveryCController> _logger;
+
+        public DiscoveryCController(ILogger<DiscoveryCController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public string Get()
+        {
+            _logger.LogInformation("begin discovery");
+
+            Uri baseUrlB = new Uri("http://localhost:5001/DiscoveryB");
+            IRestClient clientB = new RestClient(baseUrlB);
+            IRestRequest requestB = new RestRequest(Method.GET);
+            IRestResponse<string> responseB = clientB.Execute<string>(requestB);
+
+            string status;
+
+            if (responseB.IsSuccessful)
+            {
+               status = "Service Online";
+            }
+            else
+            {
+                status = responseB.ErrorMessage;
+            }
+
+            Console.WriteLine(status);
+
+            _logger.LogInformation("end discovery");
+            return status;
+        }
+    }
+}
